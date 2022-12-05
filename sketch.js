f23b67515bc1545b2821f2c33bdb2179a7623bdf
= <=>
2022-12-05 16:38:29 +0000

    Full Responsive Test

--- sketch.js
+++ sketch.js
@@ -46,16 +46,19 @@ function setup() {
     // text('Label', width * 0.5, height * 0.5);
 }
 
-function windowResized() {
-    var cnv = resizeCanvas(windowHeight * 0.8, windowHeight * 0.8)
+function windowResize() {
+    cnv = createCanvas(windowHeight * 0.8, windowHeight * 0.8);
 
 }
+
+
+
 
 
 function drawMap(_fillBool, _fillR, _fillG, _fillB, _borderBool, _borderR, _borderG, _borderB) {
     // let pointScale = 1.35;
     //heightwidth716
-    let pointScale = (height * width) / 400000;
+    let pointScale = (height * width) / 400000 - 0.1;
 
     // ===== CLEAR MAP =====
     fill(71, 105, 204);
@@ -67,16 +70,13 @@ function drawMap(_fillBool, _fillR, _fillG, _fillB, _borderBool, _borderR, _bord
     // // ===== ENABLE FOR ALIGNING CONTOUR =====
     // // =======================================
 
-    // // Change this translation to move the plan. Make sure to copy to undo translation.
-    // translate(width * 0.225 * pointScale, height * 0.05 * pointScale);
     // fill(220);
     // beginShape();
     // for (let i = 0; i < contourPoints.points.length; i++) {
-    //     vertex(contourPoints.points[i][0] * pointScale, contourPoints.points[i][1] * pointScale);
+    //     vertex((contourPoints.points[i][0] + 148) * pointScale, (contourPoints.points[i][1] + 32) * pointScale);
     // }
     // endShape();
-    // Undo Translation, Reset back to "Anchor".
-    // translate(-(width * 0.225 * pointScale), -(height * 0.05 * pointScale));
+
 
 
 
@@ -88,23 +88,18 @@ function drawMap(_fillBool, _fillR, _fillG, _fillB, _borderBool, _borderR, _bord
 
     if (_borderBool == true) {
         stroke(_borderR, _borderG, _borderB);
-        strokeWeight(2);
+        strokeWeight(3);
     } else {
         stroke(0, 0, 0);
-        strokeWeight(1);
+        strokeWeight(0);
 
     }
 
-    translate(width * 0.5, height * 0.5)
+    translate(width * 0.6, height * 0.7)
 
     // ===========================
     // ===== ENGLAND CONTOUR =====
     // ===========================
-
-
-    // Translation, don't touch constant values.
-    // Origin Translation, constants affect where all the components are rendered.
-    // translate(width * 0.2 * pointScale, height * 0.23 * pointScale);
 
     if (enContourPoints.points.length > 0) {
         beginShape();
@@ -114,66 +109,51 @@ function drawMap(_fillBool, _fillR, _fillG, _fillB, _borderBool, _borderR, _bord
         }
         endShape();
     }
-    // Undo Translation, Reset back to "Anchor".
-    // translate(-(width * 0.2 * pointScale), -(height * 0.23 * pointScale));
-    pop();
+
 
     // =========================
     // ===== WALES CONTOUR =====
     // =========================
 
-    // Translation, don't touch constant values.
-    // Translations are persistent, so this moves a certain distance from the "Anchor".
-
     if (waContourPoints.points.length > 0) {
         beginShape();
         for (let i = 0; i < waContourPoints.points.length; i++) {
+            // Don't touch constant values as they're used for alignment.
             vertex((waContourPoints.points[i][0] - 72) * pointScale, (waContourPoints.points[i][1] + 8) * pointScale);
         }
         endShape();
     }
-    // Undo Translation, Reset back to "Anchor".
-    // translate(-(width * 0.22 * pointScale), -(height * 0.43 * pointScale));
 
-    translate(width, height);
 
     // ===========================
     // ===== SCOTLAND CONTOUR ====
     // ===========================
 
-    // Translation, don't touch constant values.
-    // Translations are persistent, so this moves a certain distance from the "Anchor".
-    // translate(-(width * 0.054 * pointScale), -(height * 0.248 * pointScale));
     if (scContourPoints.points.length > 0) {
         beginShape();
         for (let i = 0; i < scContourPoints.points.length; i++) {
             
-            vertex(scContourPoints.points[i][0] * pointScale, scContourPoints.points[i][1] * pointScale);
+            vertex((scContourPoints.points[i][0] - 98) * pointScale, (scContourPoints.points[i][1] - 246) * pointScale);
         }
         endShape();
 
     }
-    // Undo Translation, Reset back to "Anchor".
-    // translate(width * 0.054 * pointScale, height * 0.248 * pointScale);
+
 
 
     // ===================================
     // ===== NORTHERN IRELAND CONTOUR ====
     // ===================================
 
-    // Translation, don't touch constant values.
-    // Translations are persistent, so this moves a certain distance from the "Anchor".
-    // translate(-(width * 0.12 * pointScale), height * 0.05 * pointScale);
+
     if (niContourPoints.points.length > 0) {
         beginShape();
         for (let i = 0; i < niContourPoints.points.length; i++) {
-            vertex(niContourPoints.points[i][0] * pointScale, niContourPoints.points[i][1] * pointScale);
+            vertex((niContourPoints.points[i][0] - 170) * pointScale, (niContourPoints.points[i][1] - 118) * pointScale);
         }
         endShape();
 
     }
-    // Undo Translation, Reset back to "Anchor".
-    // translate(width * 0.12 * pointScale, -(height * 0.05 * pointScale));
 
 
 
@@ -184,7 +164,8 @@ function draw() {
     if (mouseY >= 0 && mouseY <= height && mouseX >= 0 && mouseX <= width) {
 
 
-        drawMap(true, 255, 255, 255, true, 204, 71, 71);
+        // drawMap(true, 255, 255, 255, true, 204, 71, 71);
+        drawMap(true, 255, 255, 255, true, 68, 61, 71);
 
     } else {
 
@@ -203,7 +184,3 @@ function mouseMoved() {
 
 }
 
-function windowResize() {
-    createCanvas(windowWidth * 0.8, windowHeight * 0.8);
-
-}
--- util/meanCoordinates.py
+++ util/meanCoordinates.py
@@ -1,9 +1,11 @@
-f = open("./assets/temp.txt", "r");
+f = open("./input.txt", "r");
 
 xMean = 0;
 yMean = 0;
 count = 0;
 splitArr = []
+
+print("[✓] Starting")
 
 for line in f:
     lineSplit = line.split("\n")[0].split(",")
@@ -19,3 +21,4 @@ print("xMean: " + str(xMean) + " yMean: " + str(yMean));
 w = open("./output.txt", "w");
 for line in splitArr:
     w.write("[" + str(int(line[0]) - xMean) + "," + str(int(line[1]) - yMean) + "],");
+print("[✓] Done")
