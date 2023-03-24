205a66e45b1f2a32c10f38931d5031ad15511c97
tarteel2 <tuti.alkaraan.22@gmail.com>
2023-03-24 14:18:37 +0000

    commit changes

--- Backend/webapp.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, abort, render_template
-from jinja2 import TemplateNotFound
-from blueprints.index import index_page
-
-app = Flask(__name__)
-app.register_blueprint(index_page)
-
-app.run()
