391d33fd16d35ce8d1288a4cb2c44c6d7fccf2c1
hazzakak <smith.harry1807@hotmail.co.uk>
2023-05-06 19:51:24 +0100

    Added final api point

--- Backend/flask_app/app/api/api.py
+++ Backend/flask_app/app/api/api.py
@@ -101,3 +101,100 @@ def api_data_year_averages():
         }
     }
     return rtn_json
+
+@bp.route('/api/data_full_year_values')
+def api_data_full_year_averages():
+    year = int(request.args.get('year'))
+    country = str(request.args.get('country'))
+
+    if year < 1910 or year > 2022:
+        response = current_app.response_class(
+        response="Year must be between 1910 and 2022",
+        status=400,
+        mimetype='application/json')
+        return response
+    
+    if country not in ['en', 'wa', 'ni', 'sc']:
+        response = current_app.response_class(
+        response="Country must be en, wa, ni or sc",
+        status=400,
+        mimetype='application/json')
+        return response
+    
+    db_obj = None
+    
+    if country == "en":
+        db_obj = en.query.filter_by(year=year).first()
+    elif country == "sc":
+        db_obj = sc.query.filter_by(year=year).first()
+    elif country == "wa":
+        db_obj = wa.query.filter_by(year=year).first()
+    elif country == "ni":
+        db_obj = ni.query.filter_by(year=year).first()
+
+    rtn_json = {
+        "year": year,
+        "country": country,
+        "rainfall": {
+            "jan": db_obj.janRainfall,
+            "feb": db_obj.febRainfall,
+            "mar": db_obj.marRainfall,
+            "apr": db_obj.aprRainfall,
+            "may": db_obj.mayRainfall,
+            "jun": db_obj.junRainfall,
+            "jul": db_obj.julRainfall,
+            "aug": db_obj.augRainfall,
+            "sep": db_obj.sepRainfall,
+            "oct": db_obj.octRainfall,
+            "nov": db_obj.novRainfall,
+            "dec": db_obj.decRainfall
+        },
+
+        "sunshine": {
+            "jan": db_obj.janSunshine,
+            "feb": db_obj.febSunshine,
+            "mar": db_obj.marSunshine,
+            "apr": db_obj.aprSunshine,
+            "may": db_obj.maySunshine,
+            "jun": db_obj.junSunshine,
+            "jul": db_obj.julSunshine,
+            "aug": db_obj.augSunshine,
+            "sep": db_obj.sepSunshine,
+            "oct": db_obj.octSunshine,
+            "nov": db_obj.novSunshine,
+            "dec": db_obj.decSunshine
+        },
+
+        "highTemp": {
+            "jan": db_obj.janHighTemp,
+            "feb": db_obj.febHighTemp,
+            "mar": db_obj.marHighTemp,
+            "apr": db_obj.aprHighTemp,
+            "may": db_obj.mayHighTemp,
+            "jun": db_obj.junHighTemp,
+            "jul": db_obj.julHighTemp,
+            "aug": db_obj.augHighTemp,
+            "sep": db_obj.sepHighTemp,
+            "oct": db_obj.octHighTemp,
+            "nov": db_obj.novHighTemp,
+            "dec": db_obj.decHighTemp
+        },
+
+        "lowTemp": {
+            "jan": db_obj.janLowTemp,
+            "feb": db_obj.febLowTemp,
+            "mar": db_obj.marLowTemp,
+            "apr": db_obj.aprLowTemp,
+            "may": db_obj.mayLowTemp,
+            "jun": db_obj.junLowTemp,
+            "jul": db_obj.julLowTemp,
+            "aug": db_obj.augLowTemp,
+            "sep": db_obj.sepLowTemp,
+            "oct": db_obj.octLowTemp,
+            "nov": db_obj.novLowTemp,
+            "dec": db_obj.decLowTemp
+        }
+
+        
+    }
+    return rtn_json
